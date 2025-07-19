5b1667d3ca75e5dde25fae8ab89700872ec8e612
sashi.ethington <sashi.ethington@gmail.com>
2025-07-19 22:05:12 +0000

    updated the name of the app in pubspec.yaml file and changed the test file to match the new name

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:baby_food_nutrition_app/main.dart';
+import 'package:little_bites/main.dart';
 
 void main() {
   testWidgets('Baby Food Nutrition App smoke test', (WidgetTester tester) async {
